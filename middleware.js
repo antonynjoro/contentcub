4e5cec529ea81a3a14939511d24d407ce86aaf0b
Antony Njoroge Mutharia <antonynjoro@gmail.com>
2023-11-09 01:31:39 -0600

    Fixing middleware to allow webooks/create-user

--- middleware.js
+++ middleware.js
@@ -3,10 +3,11 @@ import { authMiddleware } from "@clerk/nextjs";
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({});
+export default authMiddleware({
+  // "/" will be accessible to all users
+  publicRoutes: ["/api/webhooks/create-user"],
+});
  
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-  publicRoutes: ["/api/webhooks/create-user"],
+      matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
- 
