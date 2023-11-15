52935ed9ce0530e00edffb4153f17049c609d027
Antony Njoroge Mutharia <antonynjoro@gmail.com>
2023-11-15 00:46:57 -0600

    Added a Landing page and made it publicly accessible

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,20 @@ export default withUt({
       fontFamily: {
         sans: ['var(--font-fira_sans)'],
       },
+      colors:{
+        gold: {
+          50: '#ffe169',
+          100: '#fad643',
+          200: '#edc531',
+          300: '#dbb42c',
+          400: '#c9a227',
+          500: '#b69121',
+          600: '#a47e1b',
+          700: '#926c15',
+          800: '#805b10',
+          900: '#76520e',
+        },
+      }
     },
   },
   plugins: [
