94d2b36ddc4e04f459851ef0c0afab633f72500f
Antony Njoroge Mutharia <antonynjoro@gmail.com>
2023-11-23 23:45:45 -0600

    slight edit to the add question modal.

--- app/components/AddQuestionModal.jsx
+++ app/components/AddQuestionModal.jsx
@@ -10,7 +10,6 @@ import Button from "./Button";
 import ModalContainer from "./ModalContainer";
 import saveQuestion from "../actions/saveQuestion";
 
-
 const questionTypes = {
   textShort: {
     description: "Short Text",
@@ -39,7 +38,11 @@ const questionTypes = {
   },
 };
 
-export default function AddQuestionModal({ handleModalClose, setQuestions, requestId }) {
+export default function AddQuestionModal({
+  handleModalClose,
+  setQuestions,
+  requestId,
+}) {
   const [selectedQuestionType, setSelectedQuestionType] = useState(undefined);
   const [questionTypehasError, setQuestionTypehasError] = useState(false);
   const [questionTitlehasError, setQuestionTitlehasError] = useState(false);
@@ -67,88 +70,88 @@ export default function AddQuestionModal({ handleModalClose, setQuestions, reque
       return;
     } else {
       saveQuestion(requestId, question)
-      .then((res) => {
-        console.log(res);
-        setQuestions((prevState) => [...prevState, res]);
-      }
-      ).catch((err) => {
-        console.log(err);
-        // in case of error, add the question to the list anyway
-        setQuestions((prevState) => [...prevState, question]);
-      }
-      );
+        .then((res) => {
+          console.log(res);
+          setQuestions((prevState) => [...prevState, res]);
+        })
+        .catch((err) => {
+          console.log(err);
+          // in case of error, add the question to the list anyway
+          setQuestions((prevState) => [...prevState, question]);
+        });
       handleModalClose();
     }
   }
   return (
     <ModalContainer handleModalClose={handleModalClose}>
-        <h3 className="text-2xl font-bold">Add a new question</h3>
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-col gap-1">
-            <div
-              className={`flex flex-wrap gap-2 pb-5
+      <h3 className="text-2xl font-bold">Add a new checklist item</h3>
+      <div className="flex flex-col gap-5">
+        <div className="flex flex-col gap-1">
+          <div
+            className={`flex flex-wrap gap-2 pb-5
           ${questionTypehasError && "rounded-md border-2 border-red-500 p-2"}
           `}
-            >
-              {Object.keys(questionTypes).map((type) => {
-                const IconComponent = questionTypes[type].icon;
-                return (
-                  <button
-                    key={type}
-                    className={`flex items-center gap-2 px-4 py-2 ${
-                      questionTypes[type].colorClass
-                    } ${
-                      selectedQuestionType === type &&
-                      "outline-3 outline outline-offset-2 outline-black "
-                    } rounded-md`}
-                    onClick={() => {
-                      setQuestion((prevState) => ({ ...prevState, type }));
-                      setSelectedQuestionType(type);
-                      setQuestionTypehasError(false);
-                    }}
-                  >
-                    <IconComponent className="h-6 w-6" />
-                    <p>{questionTypes[type].description}</p>
-                  </button>
-                );
-              })}
-            </div>
-            {questionTypehasError && (
-              <p className="text-sm text-red-700">
-                Please select a question type
-              </p>
-            )}
+          >
+            {Object.keys(questionTypes).map((type) => {
+              const IconComponent = questionTypes[type].icon;
+              return (
+                <button
+                  key={type}
+                  className={`flex items-center gap-2 px-4 py-2 ${
+                    questionTypes[type].colorClass
+                  } ${
+                    selectedQuestionType === type &&
+                    "outline-3 outline outline-offset-2 outline-black "
+                  } rounded-md`}
+                  onClick={() => {
+                    setQuestion((prevState) => ({ ...prevState, type }));
+                    setSelectedQuestionType(type);
+                    setQuestionTypehasError(false);
+                  }}
+                >
+                  <IconComponent className="h-6 w-6" />
+                  <p>{questionTypes[type].description}</p>
+                </button>
+              );
+            })}
           </div>
-          <ShortAnswerField
-            label={"Question"}
-            handleChange={(value) => {
-              setQuestion((prevState) => ({ ...prevState, title: value }));
-              setQuestionTitlehasError(false);
-            }}
-            hasError={questionTitlehasError}
-            autoFocus={true}
-            value={question.title}
-          />
-          <ShortAnswerField
-            label={"Description"}
-            handleChange={(value) =>
-              setQuestion((prevState) => ({
-                ...prevState,
-                description: value,
-              }))
-            }
-            value={question.description}
-          />
-          <div className="flex justify-end gap-2">
-            <Button
-              handleClick={() => handleModalClose()}
-              isSecondary={true}
-            >
-              Cancel
-            </Button>
-            <Button handleClick={handleClickAddButton}>Add</Button>
-          </div>
+          {questionTypehasError && (
+            <p className="text-sm text-red-700">
+              Please select a question type
+            </p>
+          )}
         </div>
-      </ModalContainer>
+        <ShortAnswerField
+          label={"Title"}
+          handleChange={(value) => {
+            setQuestion((prevState) => ({ ...prevState, title: value }));
+            setQuestionTitlehasError(false);
+          }}
+          hasError={questionTitlehasError}
+          autoFocus={true}
+          value={question.title}
+          helpText={"What kind of information are you asking for?"}
+        />
+        <ShortAnswerField
+          label={"Description"}
+          handleChange={(value) =>
+            setQuestion((prevState) => ({
+              ...prevState,
+              description: value,
+            }))
+          }
+          value={question.description}
+          helpText={
+            "Use this to describe in more detail what you are asking for."
+          }
+        />
+        <div className="flex justify-end gap-2">
+          <Button handleClick={() => handleModalClose()} isSecondary={true}>
+            Cancel
+          </Button>
+          <Button handleClick={handleClickAddButton}>Add</Button>
+        </div>
+      </div>
+    </ModalContainer>
   );
 }
