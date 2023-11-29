33aae603063956da52321f347c90134434753de2
Antony Njoroge Mutharia <antonynjoro@gmail.com>
2023-11-28 22:03:30 -0600

    added tracking for user sign in

--- app/(pages)/sign-in/[[...sign-in]]/page.jsx
+++ app/(pages)/sign-in/[[...sign-in]]/page.jsx
@@ -1,14 +1,34 @@
 "use client";
 import { SignIn } from "@clerk/nextjs";
 import { useUser } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
+import { useEffect } from 'react';
+import mixpanel from 'mixpanel-browser';
 
 export default function SignInPage() {
   const { isSignedIn, user } = useUser();
-  const router = useRouter();
 
-  if (isSignedIn) {
-    router.push("/checklists");
-    return <div>Redirecting...</div>;
-  } else return <SignIn afterSignInUrl="/checklists" />;
+  useEffect(() => {
+    // Track sign-in event with Mixpanel when user is signed in
+    if (isSignedIn) {
+      mixpanel.track('User Signed In', {
+        userId: user.id, // anonymize ID if necessary
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.emailAddresses[0].emailAddress,
+
+      });
+    }
+  }, [isSignedIn, user]);
+
+  // Clerk's SignIn component handles the redirection after sign-in.
+  // Users are always redirected to '/checklists' after signing in.
+  return (
+    <div>
+      {isSignedIn ? (
+        <div>Redirecting...</div> // This will show briefly before Clerk redirects the user
+      ) : (
+        <SignIn afterSignInUrl="/checklists" />
+      )}
+    </div>
+  );
 }
