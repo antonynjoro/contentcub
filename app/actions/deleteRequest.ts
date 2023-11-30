6b301e5a9b798fca7ca4216e4da4ae67653849fb
Antony Njoroge Mutharia <antonynjoro@gmail.com>
2023-11-29 18:41:19 -0600

    fixed delete request to ensure questions are removed and request removed from client

--- app/actions/deleteRequest.ts
+++ app/actions/deleteRequest.ts
@@ -14,12 +14,65 @@ export default async function deleteRequest(requestId: string) {
   }
   try {
     console.log("Deleting request", requestId);
-    const request = await prisma.request.delete({
-      where: {
-        id: requestId,
-      },
-    });
-    return request;
+
+    // Prisma transaction to delete questions then the request
+    await prisma.$transaction(async (prisma) => {
+      // Delete the questions associated with the request
+      await prisma.question.deleteMany({
+        where: {
+          requestId,
+        },
+      });
+
+      // Remove request from client
+      // model Client {
+      //   id        String   @id @default(auto()) @map("_id") @db.ObjectId
+      //   externalId String?   @unique
+      //   email     String   @unique
+      //   firstName      String?
+      //   lastName       String?
+      //   requestIds  String[]  @db.ObjectId
+      //   requests    Request[] @relation(fields: [requestIds], references: [id])
+      //   user      User     @relation(fields: [userId], references: [id])
+      //   userId    String   @map("userId") @db.ObjectId
+      //   attributes Json?
+      //   createdAt DateTime @default(now())
+      //   updatedAt DateTime @updatedAt
+      // }
+
+      const client = await prisma.client.findFirst({
+        where: {
+          requestIds: {
+            has: requestId
+          }
+        }
+      });
+
+      if (client) {
+        await prisma.client.update({
+          where: {
+            id: client.id
+          },
+          data: {
+            requestIds: {
+              set: client.requestIds.filter((id) => id !== requestId)
+            }
+          }
+        });
+      }
+
+
+      // Delete the request
+      await prisma.request.delete({
+        where: {
+          id: requestId,
+        },
+      });
+
+      return  
+    }
+    );
+    
   } catch (error) {
     console.error(error);
     return null;
